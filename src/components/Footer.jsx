509955d4f6a3af127fbc0b6055fcd547a8e8ba78
Fahri Febriandika Pamungkas <127507340+febrian2108@users.noreply.github.com>
2024-11-11 14:05:20 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <>
-        </>
+        <div className="note-app__footer">
+            <center>
+                Copyright &#169; 2024,{" "}
+                <a
+                    className="link"
+                    target="_blank"
+                    rel="Xxxxxxxx"
+                    href="https://github.com/febrian2108"
+                >
+                    Febrian2108
+                </a>{" "}
+            </center>
+        </div>
     );
 };
 
